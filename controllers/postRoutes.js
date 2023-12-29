089ed3a90f0b53e53a91b1c9950e9bf7a659765b
Logan Lagrange <lagrangelogan@gmail.com>
2023-12-29 14:19:55 -0500

    created post route

---
+++ controllers/postRoutes.js
@@ -0,0 +1,11 @@
+const express = require(`express`);
+const router = express.Router();
+const {Post} = require(`../models`);
+
+router.get(`/`, (req,res)  => {
+    Post.findAll().then(dbPost => {
+        res.json(dbPost);
+    }).catch(err => {
+        res.status(500).json({msg:`Server error!`, err});
+    })
+})
